cb20caec2a697adcab82ef263183908c5c73e8f8
Ebrahim Ghafourzadeh <90109369+E-Ghafour@users.noreply.github.com>
2023-08-27 01:52:15 +0330

    add more api_key

--- data/api_dict.py
+++ data/api_dict.py
@@ -2,4 +2,5 @@ api_dict = {1: "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",
             2: "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",
             3: "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",
             4: "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",
-            5: "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"}
+            5: "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",
+            6: "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"}
